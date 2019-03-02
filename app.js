7524cce96eac50d50ae917db015cfea01fab23fd
Ro5aline <wanlinlirosaline@gmail.com>
2019-03-02 18:00:42 -0500

    hardcode port

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
